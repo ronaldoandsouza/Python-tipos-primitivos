badfa52fa6d1395b22d38d80d998fb5d1ef98e42
Ronaldo Souza <ronaldoandsouza12@gmail.com>
2025-05-17 10:34:26 -0300

    primeiro commit

---
+++ ex003.py
@@ -0,0 +1,6 @@
+'''Desafio Python 003: Crie um script que leia dois números e mostre a soma entre eles'''
+
+n1 = int(input('Enter a number: '))
+n2 = int(input('Enter another number: '))
+s = n1 + n2
+print(f'The sum of {n1} and {n2} is equal to: {s}')
